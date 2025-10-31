f8ca2cbe736e919c74b8b149299050c90fdac910
Perry <aidanpj18@gmail.com>
2025-10-31 09:18:13 -0400

    Remove obsolete docs and test fixtures

--- temp_fix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Just fix lines 14-16
-sed -i.bak11 '14s/.*/    INSTALL_NAME="raylib_lua.dylib"/' install_local.sh
-sed -i.bak12 '16s/.*/    INSTALL_NAME="raylib_lua.so"/' install_local.sh
