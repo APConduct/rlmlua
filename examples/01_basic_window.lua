30db59314138daead6a9c23a327223776521823d
Perry <aidanpj18@gmail.com>
2025-10-31 16:03:28 -0400

    Format basic window example calls and export mla

--- examples/01_basic_window.lua
+++ examples/01_basic_window.lua
@@ -4,12 +4,16 @@ local rlc = rl.colors
 local screen_width = 800
 local screen_height = 450
 
-local window = rl.init_window(screen_width, screen_height, "rlmlua - Basic Window")
+local window = rl.init_window(
+    screen_width, screen_height,
+    "rlmlua - Basic Window")
 window:set_target_fps(60)
 
 while not window:should_close() do
     window:begin_drawing()
     window:clear_background(rlc.RAYWHITE)
-    window:draw_text("Congrats! You created your first window!", 190, 200, 20, rlc.LIGHTGRAY)
+    window:draw_text(
+        "Congrats! You created your first window!",
+        190, 200, 20, rlc.LIGHTGRAY)
     window:end_drawing()
 end
